d3df3204713e41e1c16b3648812d7ca5ab5828c5
Antoine Durussel <antoine.durussel@gmail.com>
2020-01-14 13:58:57 +0100

    FEAT groupstage

--- angular/src/app/schedule/groupstage/groupstage.component.ts
+++ angular/src/app/schedule/groupstage/groupstage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GROUPS } from '../../mock-groups';
 
 @Component({
   selector: 'app-groupstage',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groupstage.component.scss']
 })
 export class GroupstageComponent implements OnInit {
+
+  groups = GROUPS;
 
   constructor() { }
 
